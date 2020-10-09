8a365f1d8669d5f6724caee0cc30a33cb2250ac8
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-08 21:20:20 -0300

    :lipstick: feat: Login page created

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+// components
+import Input from "../../components/Input";
+// imgs
+import logoImg from "../../assets/logo.svg";
+// styles
+import { LoginContainer, LoginForm } from "./styles";
+import { Button } from "../../styles/objects/button";
+
+const Login = () => {
+  return (
+    <main>
+      <LoginContainer>
+        <div></div>
+        <LoginForm>
+          <img src={logoImg} alt="Nave.rs" />
+          <Input label="E-mail" name="email" type="email" required />
+          <Input label="Senha" name="password" type="password" required />
+          <Button type="submit">Entrar</Button>
+        </LoginForm>
+      </LoginContainer>
+    </main>
+  );
+};
+
+export default Login;
---
+++ src/pages/Login/styles.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+// components
+import { Container } from "../../styles/objects/grid";
+
+export const LoginContainer = styled(Container)`
+  & {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    place-content: center;
+    flex-direction: column;
+    height: 100vh;
+  }
+`;
+
+export const LoginForm = styled.form`
+  & {
+    width: 100%;
+    padding: calc(var(--gap) * 5) var(--gap-md);
+    border: 1px solid var(--gray-900);
+
+    img {
+      margin: 0 auto calc(var(--gap) * 5) auto;
+    }
+
+    input {
+      margin-bottom: var(--gap-md);
+    }
+  }
+`;
