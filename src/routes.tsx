bd04fb8865cc200a2d3a619587d154e81c70e27b
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-09 20:51:01 -0300

    :construction: chore: edit naver route added

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import AddNaver from "./pages/Add-naver";
+import EditNaver from "./pages/Edit-naver";
 
 const Routes = () => {
   return (
@@ -13,6 +14,7 @@ const Routes = () => {
         <Route path="/" exact component={Login} />
         <Route path="/index" component={Home} />
         <Route path="/add-naver" component={AddNaver} />
+        <Route path="/edit-naver/:id" component={EditNaver} />
       </Switch>
     </BrowserRouter>
   );
