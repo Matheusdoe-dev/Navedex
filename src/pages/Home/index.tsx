d6fa699325241872e9e925fd5d7ee0c311ecb73f
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-09 21:03:36 -0300

    :construction: chore: Home page updates
    Modal component imported, useModal custom hook imported, HomeBody styled component created, modals created (naver-detail, naver-deleted and naver-confirm-delete)

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,46 +3,126 @@ import React from "react";
 import Header from "../../components/Header";
 import Naver from "../../components/Naver-card";
 // styles
-import { HomeContainer, HomeHeader, Navers } from "./styles";
+import {
+  HomeContainer,
+  HomeHeader,
+  Navers,
+  HomeBody,
+  ModalButtonsBlock,
+  CloseButton,
+} from "./styles";
 import { Button } from "../../styles/objects/button";
 // imgs
 import naverImg from "../../assets/naver_4.jpg";
+import Modal from "../../components/Modal";
+import naverDetailImg from "../../assets/naver_detail.jpg";
+import closeIcon from "../../assets/close-icon.svg";
+// hooks
+import useModal from "../../hooks/modal";
+import ModalNaverDetail from "../../components/Modal-Naver-Detail";
 
 const Home = () => {
+  const { handleActive, handleInactive } = useModal();
+
   return (
     <>
-      <Header />
+      <HomeBody data-anime="modal" onClick={handleInactive}>
+        <Header />
 
-      <main>
-        <HomeContainer>
-          <HomeHeader>
-            <h1>Navers</h1>
-            <Button width="auto">Adicionar Naver</Button>
-          </HomeHeader>
-          <Navers>
-            <Naver
-              name="Juliano Reis"
-              role="Front-end Developer"
-              image={naverImg}
-            />
-            <Naver
-              name="Juliano Reis"
-              role="Front-end Developer"
-              image={naverImg}
-            />
-            <Naver
-              name="Juliano Reis"
-              role="Front-end Developer"
-              image={naverImg}
-            />
-            <Naver
-              name="Juliano Reis"
-              role="Front-end Developer"
-              image={naverImg}
-            />
-          </Navers>
-        </HomeContainer>
-      </main>
+        <main>
+          <HomeContainer>
+            <HomeHeader>
+              <h1>Navers</h1>
+              <Button to="/add-naver" width="11rem">
+                Adicionar Naver
+              </Button>
+            </HomeHeader>
+            <Navers>
+              <Naver
+                name="Juliano Reis"
+                role="Front-end Developer"
+                image={naverImg}
+                deleteFunction={() => handleActive("delete")}
+                showFunction={() => handleActive("naver-detail")}
+              />
+              <Naver
+                name="Juliano Reis"
+                role="Front-end Developer"
+                image={naverImg}
+                deleteFunction={() => handleActive("delete")}
+                showFunction={() => handleActive("naver-detail")}
+              />
+              <Naver
+                name="Juliano Reis"
+                role="Front-end Developer"
+                image={naverImg}
+                deleteFunction={() => handleActive("delete")}
+                showFunction={() => handleActive("naver-detail")}
+              />
+              <Naver
+                name="Juliano Reis"
+                role="Front-end Developer"
+                image={naverImg}
+                deleteFunction={() => handleActive("delete")}
+                showFunction={() => handleActive("naver-detail")}
+              />
+            </Navers>
+          </HomeContainer>
+        </main>
+      </HomeBody>
+
+      {/* modals */}
+      {/* naver confirm delete modal */}
+      <Modal
+        title="Excluir Naver"
+        content="Tem certeza que deseja excluir este Naver?"
+        modal="delete"
+      >
+        <ModalButtonsBlock>
+          <Button
+            as="button"
+            width="11rem"
+            background="white"
+            color="var(--gray-900)"
+            border="1px solid var(--gray-900)"
+            onClick={handleInactive}
+          >
+            Cancelar
+          </Button>
+          <Button
+            as="button"
+            width="11rem"
+            onClick={() => handleActive("deleted")}
+          >
+            Excluir
+          </Button>
+        </ModalButtonsBlock>
+      </Modal>
+
+      {/* naver deleted modal */}
+      <Modal
+        title="Naver excluído"
+        content="Naver excluído com sucesso!"
+        modal="deleted"
+      >
+        <CloseButton as="button" onClick={handleInactive}>
+          <img src={closeIcon} alt="Close" />
+        </CloseButton>
+      </Modal>
+
+      {/* naver detail modal */}
+      <ModalNaverDetail
+        naverName="Juliano Reis"
+        role="Front-end Developer"
+        age="27"
+        companyTime="3 anos"
+        projects={5}
+        image={naverDetailImg}
+      >
+        <CloseButton as="button" onClick={handleInactive}>
+          <img src={closeIcon} alt="Close" />
+        </CloseButton>
+      </ModalNaverDetail>
     </>
   );
 };
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,5 +1,18 @@
 import styled from "styled-components";
+// components
 import { Container } from "../../styles/objects/grid";
+import { Button } from "../../styles/objects/button";
+
+export const HomeBody = styled.div`
+  & {
+    height: 100vh;
+
+    &.modal {
+      background: var(--gray-900);
+      filter: brightness(50%);
+    }
+  }
+`;
 
 export const HomeContainer = styled(Container)`
   & {
@@ -22,5 +35,39 @@ export const Navers = styled.section`
     grid-template-columns: 1fr 1fr 1fr 1fr;
     column-gap: var(--gap-md);
     row-gap: var(--gap-md);
+  }
+`;
+
+export const ModalButtonsBlock = styled.div`
+  & {
+    display: flex;
+    justify-content: flex-end;
+    margin-top: calc(var(--gap) * 5);
+
+    button:last-child {
+      margin-left: calc(var(--gap) * 3);
+    }
+  }
+`;
+
+export const CloseButton = styled(Button)`
+  & {
+    display: flex;
+    place-content: center;
+    position: absolute;
+    width: 24px;
+    height: 24px;
+    left: 94%;
+    top: 5%;
+    z-index: 901;
+    background: none;
+    color: none;
+
+    img {
+      position: relative;
+      top: -12px;
+      width: 24px;
+      height: 24px;
+    }
   }
 `;
