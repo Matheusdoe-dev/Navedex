f5c81d4e58e197965bb6bf13e77d5477570c0df4
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-13 11:13:48 -0300

    :sparkles: feat: useLogin custom hook created
    hook to handle user login feature

---
+++ src/hooks/login.ts
@@ -0,0 +1,56 @@
+import { useState, FormEvent, useEffect, useContext } from "react";
+import { useHistory } from "react-router-dom";
+// apis
+import navedexApi from "../services/navedexApi";
+// hooks
+import { ModalContext } from "./modal";
+
+export const useLogin = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // error states
+  const [credentialsError, setCredentialsError] = useState(false);
+
+  const history = useHistory();
+
+  const modalContext = useContext(ModalContext);
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    setCredentialsError(false);
+
+    const response = await navedexApi
+      .post("users/login", {
+        email,
+        password,
+      })
+      .then((r) => r.data)
+      .catch(() => {
+        setCredentialsError(true);
+      });
+
+    if (response) {
+      localStorage.setItem("token", `Bearer ${response.token}`);
+      history.push("/index");
+    } else {
+      // console.log(message);
+    }
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("login-error")) {
+      modalContext?.handleActive("error-access-denied");
+      localStorage.clear();
+    }
+  }, [modalContext]);
+
+  return {
+    setEmail,
+    setPassword,
+    handleLogin,
+    email,
+    password,
+    credentialsError,
+  };
+};
