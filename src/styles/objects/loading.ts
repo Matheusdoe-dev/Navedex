fcf1a1f6dc3c6d481a63faf11dcf383f3b0c646e
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-16 10:18:48 -0300

    :lipstick: feat: Loading styled component created

---
+++ src/styles/objects/loading.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+// components
+import { BarLoader } from "react-spinners";
+
+export const Loading = styled(BarLoader)`
+    border-color: var(--gray-900);
+`;
